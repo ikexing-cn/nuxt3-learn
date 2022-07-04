2d897ed893dddeec036291551e4464b0b9313f62
ikexing <ikexing@qq.com>
2022-07-04 09:41:51 +0000

    fix: pinia reload render

--- pages/course-list.vue
+++ pages/course-list.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
 import type { SizeType } from 'ant-design-vue/es/config-provider'
 import { useCount } from '~~/composables/count'
-const { inc, dec, count } = useCount()
+const count = useCount()
+const { count: countValue, inc, dec } = toRefs(count)
 
 const btnSize: SizeType = 'small'
 </script>
@@ -13,7 +14,7 @@ const btnSize: SizeType = 'small'
         <plus-circle-two-tone />
       </template>
     </a-button>
-    <div> count: {{ count }} </div>
+    <div> count: {{ countValue }} </div>
     <a-button type="primary" :size="btnSize" @click="dec()">
       <template #icon>
         <minus-circle-two-tone />
