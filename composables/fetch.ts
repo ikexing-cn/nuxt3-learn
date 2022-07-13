0581f1a0452ec1b547fcff7c9d781bbd6d4dac8f
ubuntu <ikexing@qq.com>
2022-07-13 16:23:35 +0800

    chore: test response error interceptors

--- composables/fetch.ts
+++ composables/fetch.ts
@@ -4,15 +4,19 @@ import { storeToRefs } from 'pinia'
 export const useCustomFetch = $fetch.create({
   baseURL: 'http://localhost:4000/',
   async onRequest({ options }) {
-    const input = options.method === 'get' ? options.params : options.body
+    const requestData = options.method === 'get' ? options.params : options.body
     const { count } = storeToRefs(useCount())
     options.headers = {
       ...options.headers,
       customs: `count: ${count.value}`,
     }
-    console.log('[fetch request]', input)
+    console.log('[fetch request]', requestData)
   },
   async onResponse({ response }) {
     console.log('[fetch response]', response.status, response._data)
+  },
+  async onResponseError({ request, response }) {
+    // Log error
+    console.log('[fetch response error]', request, response.status, response.body)
   },
 })
